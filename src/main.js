ea88dace6b79059f680a90aaaf325675ea302f9d
Jeremy Huang <jeremidee@gmail.com>
2019-10-16 15:50:49 +0100

    clear comment

--- src/main.js
+++ src/main.js
@@ -7,11 +7,4 @@ Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App),
-  // created () {
-  //   if (sessionStorage.redirect) {
-  //     const redirect = sessionStorage.redirect
-  //     delete sessionStorage.redirect
-  //     this.$router.push(redirect)
-  //   }
-  // }
 }).$mount('#app')

b5af2468e74ee3a2e5d56e5e6ba08d1c46eb2988
Jeremy Huang <jeremidee@gmail.com>
2019-11-06 20:33:37 +0000

    case update

--- src/views/works/politics.vue
+++ src/views/works/politics.vue
@@ -5,18 +5,34 @@
             <h1>/ canpro /</h1>
             <p class="case-intro">In Aug, 2018, when the Taipei Mayor & Legislator Election is about to start, we notice that there is a annoying problems needs to be solve. <br><b>The Political Paper</b>. </p>
             <img src="../../assets/paper.jpg" alt="">
+            <h2 class="big-title">" It is a massive gap for people to read the political paper, so most of the voters don't understand what the candidate did, or what they will do for voters. "</h2>
+            <p>To find out what and when would affect the voter decision, and what's the key factors that they don't count on the paper to make their decision, we decide to figure out the voter's decision flow.</p>
             <div class="gap"></div>
+            
             <h2>As-Is Scenario Map: What is the voter's journey ?</h2>
             <p>Hence, we collect data from mutiples interviewers, making a As-is Scenario Map to explain the whole journey while the voter get to the election.</p>
             <img src="../../assets/case1-3.png" alt="As-is Scenario Map">
             <div class="gap"></div>
+            
             <h2>The Paint Points</h2>
             <p>We choose some of the significant problems and user pain points from the As-Is journey above:</p>
             <img src="../../assets/case1-2.png" alt="user pain points">
+            <ul class="pain-points">
+                <li>Voter didn’t receive the Politics Paper before the election.</li>
+                <li>Voter feel that the Paper is too thick and hard to read, then it result in making bad decision.</li>
+                <li>Some voters focus on some specific issues, but these issues cannot  reveal on and be classified by the Paper.</li>
+                <li>Some voters needs more information about the candidates, but it cannot be concluded on the Paper.</li>
+            </ul>
             <div class="gap"></div>
+            
             <h2>Define the Information Architecture</h2>
             <p class="case-txt">After analyzing all the pain points and the voter's journey, we conclude the idea and have an Information Architecture of this new platform for the voter.</p>
-            <img src="../../assets/case1-4.png" alt="Information Architecture of platform" class="align-paragraph">
+            <img src="../../assets/case1-4.png" alt="Information Architecture of platform">
+            <div class="gap"></div>
+            
+            <h2>Logic Flow</h2>
+            <p>Then, we based on the IA, draw a logic flow of this platform, to make sure the platform meet the usability principle.</p>
+            <img src="../../assets/canpro_logicflow.svg" alt="Logic flow of this product" class="align-paragraph">
             <div class="gap"></div>
 
             <h2>Begin to Sketch</h2>
@@ -30,7 +46,44 @@
             <p>Also, the voter can easily understand the specific candidate's focused issues through the radar chart.</p>
             <img src="../../assets/case1-7.jpg" alt="">
             <div class="gap"></div>
+            <div class="figma">
+                <iframe style="border: none;" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FP9puMSKJm4aqB5OsF3Ng52%2Fcanpro%3Fnode-id%3D0%253A2%26viewport%3D-991%252C178%252C0.4206264019012451%26scaling%3Dscale-down" allowfullscreen></iframe>
+            </div>
+            <div class="gap"></div>
         </div>
         <img src="../../assets/case1.png" alt="politics case mobile show." class="full-width">
+
     </div>
 </template>
+<style lang="scss" scoped>
+    #app main .case .case-container h2.big-title {
+        grid-column: 2/12;
+        font-family: initial;
+    }
+    #app main .case .case-container {
+        .pain-points {
+            grid-column: 2/12;
+            margin: 50px 0;
+            list-style: none;
+            
+            li {
+                font-size:20px;
+                margin: 30px 0;
+                margin-left: 20px;
+                position: relative;
+                &:before {
+                    content:''; 
+                    position: absolute;
+                    display: block;
+                    width:20px;
+                    height:20px;
+                    left: -30px;
+                    
+                    background-image: url('../../assets/icon_confused.svg');
+                    background-repeat: no-repeat;
+                    background-size: 100%;
+                }
+            }
+        }
+    }
+</style>
--- src/views/works/subs.vue
+++ src/views/works/subs.vue
@@ -4,10 +4,10 @@
             <h1>/ Subscription Manager /</h1>
             <p class="case-intro">Subs! is a subscription manager which can be arrange all your online subscriptions in one place.</p>
             <img src="../../assets/case5-1.png" class="wide" alt="">
-            <!-- <p>Here is the Figma prototypes:</p>
+            <p>Here is the Figma prototypes:</p>
             <div class="figma">
-                <iframe style="border: none;" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FJZfYWOvu5FCk7HkUhkNGEb%2F%25E9%25BA%2597%25E5%25AC%25B0%25E6%2588%25BF%3Fnode-id%3D68%253A105%26viewport%3D240%252C341%252C0.06829006224870682%26scaling%3Dscale-down" allowfullscreen></iframe>
-            </div> -->
+                <iframe style="border: none;" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F4TnbuEuGfTZYrCvxTcOKs9%2Fsub%3Fnode-id%3D8%253A0%26viewport%3D419%252C-44%252C0.11194030195474625%26scaling%3Dscale-down" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
 </template>
